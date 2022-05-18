048a25f854369739a6b71f2559e3248f7b7bfab0
oussama <contact.elprofessordz@gmail.com>
2022-05-18 22:29:28 +0100

    new BottumNavbar

--- app/src/main/java/app/easyeducation/User.java
+++ app/src/main/java/app/easyeducation/User.java
@@ -1,4 +1,30 @@
 package app.easyeducation;
 
 public class User {
+
+
+    public String nom;
+    public String prenom;
+    public String password;
+    public String email;
+    public int type;
+
+    public User(String nom,String password,String email,int type)
+    {
+
+
+        this.nom=nom;
+
+        this.password=password;
+        this.email=email;
+        this.type=type;
+    }
+
+
+    //i always make one empty for errors
+    User()
+    {
+
+    }
+
 }
--- app/src/main/java/app/easyeducation/inscription.java
+++ app/src/main/java/app/easyeducation/inscription.java
@@ -10,11 +10,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class inscription extends AppCompatActivity {
  private TextView signin;
@@ -28,6 +30,7 @@ public class inscription extends AppCompatActivity {
     String Sname;
     String Semail;
     String Spassword;
+    int type;
 
  private FirebaseAuth mAuth;
 
@@ -55,15 +58,17 @@ public class inscription extends AppCompatActivity {
         
         this.var_prof=findViewById(R.id.var_prof);
         //if clicked variable==1
-
+        Sname=username.toString();
+        Semail= email.toString();
+        Spassword=password.toString();
+        if (var_etudiant.isChecked()) type=1;
+        else if (var_prof.isChecked()) type=2;
 
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Sname=username.toString();
-                Semail= email.toString();
-                Spassword=password.toString();
+
                 int passion=0;
                 if (var_etudiant.isChecked())
                 {
@@ -99,8 +104,21 @@ public class inscription extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
 
+                User newUser=new User(Sname,Spassword,Semail,type);
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(newUser).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                    //show main activity
+
+                                    Toast.makeText(inscription.this,"Successfully added ...",Toast.LENGTH_SHORT).show();
+                                }
+                            });
                         } else {
 
+                            Toast.makeText(inscription.this,"connexion failed ...",Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
